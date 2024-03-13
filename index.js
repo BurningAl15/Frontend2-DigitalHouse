851669d2ecdff74cf1ae8c66cca76d3599465a8d
aldhairveraDev <aldhairvera-externo@tgacompany.com>
2024-03-12 20:32:26 -0500

    Classes 2, 7 and 16 solutions

---
+++ index.js
@@ -0,0 +1,12 @@
+document.body.innerHTML+="<p>Hola mundo</p>"
+
+const options = {
+    1:"piedra",
+    2:"papel",
+    3:"tijera"
+}
+
+const optionSelected = parseInt(prompt("Select Opt"))
+
+console.log(options[optionSelected])
+
